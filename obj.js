bf0436de54b095e9590fab317c54e0d341f0030c
sihyoen <rhdaudtn12@naver.com>
2024-09-20 15:02:44 +0900

    new

---
+++ obj.js
@@ -0,0 +1,2 @@
+const obj = { a: 3 };
+obj.a = 5;
---
+++ script.js
@@ -0,0 +1,13 @@
+var myVariable1;
+
+let myVariable2;
+
+myVariable2 = 123;
+
+let pokerChips = 100;
+
+const MY_VARIABLE = 123;
+
+const PI = 3;
+
+
